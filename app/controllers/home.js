5cd48d6a3a81626e22a3e589ec6d3c542481ff47
Romain Lenoel <romain.lenoel@gmail.com>
2017-08-22 10:51:14 +0200

    handle reconnection to rabbitMQ and DB

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const async = require('async');
 const router = express.Router();
-const db = require('../models');
-const amqp = require('amqplib');
 
 const processingQueueName = process.env.PROCESSING_QUEUE_NAME || "processingQueue";
 
@@ -10,14 +7,10 @@ const precision = 100;
 const computeWidth = 10;
 const imageWidth = 900;
 const imageHeight= 600;
-let channel;
-let socketIO;
 
 
 module.exports = function(app) {
   app.use('/', router);
-  channel = app.get("rabbitMQChannel");
-  socketIO = app.get('socketIO');
 };
 
 router.get('/', function(req, res, next) {
@@ -27,16 +20,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/launch', function(req, res) {
+  let app = req.app;
+  let taskMessage;
   for (var i = 0; i < imageWidth / computeWidth; i++) {
-    publishNewMessage(i * computeWidth);
-    socketIO.emit('compute_task_created');
+    taskMessage = getTaskMessage(i * computeWidth);
+    app.get('rabbitMQChannel').sendToQueue(processingQueueName, taskMessage);
+    console.log(`app - [x] Sent new task ${taskMessage}`);
+    app.get('socketIO').emit('compute_task_created');
   }
   res.sendStatus(200);
 });
 
-async function publishNewMessage(col) {
+function getTaskMessage(col) {
 
-  const mandelbrotData = {
+  return Buffer.from(JSON.stringify({
     x: 0,
     y: 0,
     scaleX: 1.5,
@@ -47,13 +44,5 @@ async function publishNewMessage(col) {
     stepX : computeWidth,
     stepY : imageHeight,
     iter: precision
-  };
-
-  await channel.assertQueue(processingQueueName, {
-    durable: true
-  });
-
-  let msg = Buffer.from(JSON.stringify(mandelbrotData));
-  channel.sendToQueue(processingQueueName, msg);
-  console.log(" [x] Sent %s", msg);
+  }));
 }
